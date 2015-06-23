78602657ffa8b170373fa4f44aaab95d52f9c387
Isaac Strack <strack@gmail.com>
2015-06-23 14:55:23 -0600

    three fourths there

--- WORKING/LendLib/LendLib.js
+++ WORKING/LendLib/LendLib.js
@@ -9,6 +9,36 @@ if (Meteor.isClient) {
     i.select();
   }
   
+  function selectCategory(e,t){
+    Session.set('current_list',this._id);
+  }
+  
+  function addItem(list_id,item_name){
+    if (!item_name&&!list_id) return;
+    lists.update({_id:list_id},
+                 {$addToSet:{items:{Name:item_name}}});
+  };
+  
+  function removeItem(list_id,item_name){
+    if (!item_name&&!list_id) return;
+    lists.update({_id:list_id},
+                 {$pull:{items:{Name:item_name}}});
+  };
+  function updateLendee(list_id,item_name,lendee_name){
+    var l = lists.findOne({"_id":list_id,
+                         "items.Name":item_name});
+    if (l&&l.items){
+      for (var i = 0; i< l.items.length; i++){
+        if (l.items[i].Name === item_name){
+          var updateItem = {};
+          updateItem['items.'+i+'.LentTo'] = lendee_name;
+          lists.update({'_id':list_id},{$set:updateItem});
+          break;
+        }
+      }
+    }
+  };
+  
   Template.categories.helpers({
     lists : function(){
       return lists.find({},{sort:{Category:1}});
@@ -16,6 +46,22 @@ if (Meteor.isClient) {
     new_cat : function(){
       //return true if adding_category == true
       return Session.equals('adding_category',true);
+    },
+    list_status : function(){
+      if (Session.equals('current_list'), this._id)
+        return " btn-info";
+      else
+        return " btn-primary";
+    },
+    list_selected : function(){
+      return ((Session.get('current_list')!= null) && 
+              (!Session.equals('current_list',null)));
+    },
+    list_adding : function(){
+      return (Session.equals('list_adding',true));
+    },
+    lendee_editing : function(){
+      return (Session.equals('lendee_input',this.Name));
     }
   });
   Template.categories.events({
@@ -37,7 +83,8 @@ if (Meteor.isClient) {
     },
     'focusout #add-category' : function (e,t){
       Session.set('adding_category', false);
-    }
+    },
+    'click .category' : selectCategory
   });
   
   Template.list.helpers({
@@ -49,13 +96,54 @@ if (Meteor.isClient) {
           _id: Session.get('current_list')
         });
         if (cats && cats.items){
-          for (var i)
-        }
-      }
+          for (var i =0; i < cats.length; i++){
+            var itm = cats.items[i];
+            itm.Lendee = item.LentTo ? itm.LentTo : "free";
+            itm.LendClass = item.LentTo ? "label-danger" : "label-success";
+          }
+          return cats.items;
+        };
+      };
       
     }
+  });
+  Template.list.events({
+    'click #btnAddItem' : function (e,t){
+      Session.set('list_adding',true);
+      Tracker.flush();
+      focusText(t.find("#item_to_add"));
+    },
+    'keyup #item_to_add' : function (e,t){
+      if (e.which === 13)
+      {
+        addItem(Session.get('current_list'),e.target.value);
+        Session.set('list_adding',false);
+      }
+    },
+    'focusout #item_to_add' : function (e,t){
+      Session.set('list_adding',false);
+    },
+    'click .delete_item' : function (e,t){
+      removeItem(Session.get('current_list'),e.target.id);
+    },
+    'click .lendee' : function (e,t){
+      Session.set('lendee_input', this.Name);
+      Tracker.flush();
+      focusText(t.find("#edit_lendee"),this.LentTo);
+    },
+    'keyup #edit_lendee' : function (e,t){
+      if (e.which === 13)
+      {
+        updateLendee(Session.get('current_list'),this.Name,
+                     e.target.value);
+        Session.set('lendee_input',null);
+      }
+      if (e.which === 27)
+      {
+        Session.set('lendee_input',null);
+      }
+    }
   })
-  
 }
 
 if (Meteor.isServer) {
